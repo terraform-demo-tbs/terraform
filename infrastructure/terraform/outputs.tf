3479ce3514e5a52bb505d4ceef1c8fa3121e92b5
Borislav Gergovski <gergovski@gmail.com>
2021-03-08 12:11:59 +0200

    chore: function test

--- infrastructure/terraform/outputs.tf
+++ infrastructure/terraform/outputs.tf
@@ -10,10 +10,6 @@ output "client_certificate" {
   value = azurerm_kubernetes_cluster.demo-aks.kube_config.0.client_certificate
 }
 
-output "kube_config" {
-  value = azurerm_kubernetes_cluster.demo-aks.kube_config_raw
-}
-
 output "function_hostname" {
   value = azurerm_function_app.demo-function.default_hostname
 }
