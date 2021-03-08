704c2a714df98446869bb94da90f13e8b13845ee
Borislav Gergovski <gergovski@gmail.com>
2021-03-08 14:05:53 +0200

    chore: function test

--- function/demo-function.cs
+++ function/demo-function.cs
@@ -7,11 +7,15 @@ using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
+using Azure.Messaging.ServiceBus;
 
 namespace Company.Function
 {
     public static class DemoFunction
     {
+        static string connectionString = "Endpoint=sb://demo-namespace-dev.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=C+L8mEbSfm/aa4gU11dYu9dzoJ+vDlzCAsdEAcDROYU=";
+        static string queueName = "demo-queue-dev";
+
         [FunctionName("DemoFunction")]
         public static async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
@@ -29,7 +33,26 @@ namespace Company.Function
                 ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
                 : $"Hello, {name}. This HTTP triggered function executed successfully.";
 
+            await SendMessageAsync(name);
+
             return new OkObjectResult(responseMessage);
+        }
+
+        public static async Task SendMessageAsync(string inputMessage)
+        {
+            // create a Service Bus client 
+            await using (ServiceBusClient client = new ServiceBusClient(connectionString))
+            {
+                // create a sender for the queue 
+                ServiceBusSender sender = client.CreateSender(queueName);
+
+                // create a message that we can send
+                ServiceBusMessage message = new ServiceBusMessage(inputMessage);
+
+                // send the message
+                await sender.SendMessageAsync(message);
+                Console.WriteLine($"Sent a single message to the queue: {queueName}");
+            }
         }
     }
 }
