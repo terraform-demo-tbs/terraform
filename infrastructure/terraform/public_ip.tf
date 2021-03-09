4c9a170e5ebfbf637c198affdb2e6cc853ffd00b
Borislav Gergovski <gergovski@gmail.com>
2021-03-09 09:38:43 +0200

    chore: function test

--- infrastructure/terraform/public_ip.tf
+++ infrastructure/terraform/public_ip.tf
@@ -11,3 +11,17 @@ resource "azurerm_public_ip" "demo-public-ip" {
     department = var.department
   }
 }
+
+resource "azurerm_public_ip" "demo-public-ip2" {
+  name                = "acceptancePublicIp2-${var.environment}"
+  resource_group_name = azurerm_resource_group.demo-resource-group.name
+  location            = azurerm_resource_group.demo-resource-group.location
+  allocation_method   = "Static"
+  sku = "Standard"
+
+  tags = {
+    source = "Terraform"
+    environment    = var.environment
+    department = var.department
+  }
+}
